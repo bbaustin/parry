fa4b105963b94b7f6c2a0b75a68306a412db4d84
bbaustin <benjaminboydaustin@gmail.com>
2023-08-14 10:39:28 +0900

    Add game loop

--- scripts.js
+++ scripts.js
@@ -1,3 +1,8 @@
+/////////////
+// Arena  //
+////////////
+const arena = document.getElementById('arena');
+
 ///////////////////
 // Player Sword //
 //////////////////
@@ -16,18 +21,48 @@ document.addEventListener('mousemove', (event) => {
   sword.style.top = mouseY + yOffset + 'px';
 });
 
-let rotationAngle = 0;
-
-document.addEventListener('keydown', (event) => {
-  if (event.key === 'q' && rotationAngle >= -75) {
-    rotationAngle -= 15; // Decrease angle to rotate left
-    sword.style.transform = `rotate(${rotationAngle}deg)`;
-  } else if (event.key === 'e' && rotationAngle <= 75) {
-    rotationAngle += 15; // Increase angle to rotate right
-    sword.style.transform = `rotate(${rotationAngle}deg)`;
-  }
-});
-
 /////////////////
 // Enemy Sword //
 ////////////////
+
+///////////////
+// Game Loop //
+///////////////
+var keyState = {
+  q: false,
+  e: false,
+};
+document.addEventListener('keydown', (event) => {
+  if (event.key === 'q') {
+    keyState.q = true;
+  }
+  // Increase angle to rotate right
+  else if (event.key === 'e') {
+    keyState.e = true;
+  }
+});
+document.addEventListener('keyup', (event) => {
+  if (event.key === 'q') {
+    keyState.q = false;
+  } else if (event.key === 'e') {
+    keyState.e = false;
+  }
+});
+let rotationAngle = 0;
+function gameLoop() {
+  console.log(rotationAngle);
+  if (keyState.q && rotationAngle >= -80) {
+    // Decrease angle to rotate left
+    rotationAngle -= 10;
+    sword.style.transform = `rotate(${rotationAngle}deg)`;
+  }
+  // Increase angle to rotate right
+  if (keyState.e && rotationAngle <= 80) {
+    // Increase angle to rotate right
+    rotationAngle += 10;
+    sword.style.transform = `rotate(${rotationAngle}deg)`;
+  }
+  setTimeout(gameLoop, 10);
+}
+
+gameLoop();
